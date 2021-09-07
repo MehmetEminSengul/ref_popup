e14a7819ea46764c8228584f0f3b37bee0f49858
Mehmet Emin Şengül <m.eminsengul@icloud.com>
2021-09-07 11:04:08 +0300

    Popup Referans Project

---
+++ app/src/main/java/com/example/referanspopup/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.referanspopup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WifiManager wifiManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+    }
+
+    public void enableWifi(View v) {
+        wifiManager.setWifiEnabled(true);
+    }
+
+    public void disableWifi(View v) {
+        wifiManager.setWifiEnabled(false);
+    }
+
+}
